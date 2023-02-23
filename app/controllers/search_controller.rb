e2fd462a35e23e37541b4a5b23e17d0541b1536b
Mizut452 <ssvror444@gmail.com>
2023-02-24 03:05:55 +0900

    バグあり

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,22 @@
 class SearchController < ApplicationController
   def search
+    require 'net/http'
+    require 'json'
+
+  if poke_code = params[:poke_code]
+
+    params = URI.encode_www_form({zipcode: poke_code})
+
+
+    uri = URI.parse("https://pokeapi.co/api/v2/pokemon/#{params}")
+
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+
+  if result["id"]
+    @Poke_Id = result["id"][0]["id"]
   end
+end
+end
+
 end
